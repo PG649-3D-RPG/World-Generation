005db5440c941ef83062cff57193e6ae6891bd0c
kb1t <kb1t@users.noreply.github.com>
2022-11-09 17:33:51 +0100

    navmeshes are baked in now

--- Packages/com.pg649.worldgeneration/Runtime/Terrain Generation/Scripts/EnvironmentGeneratorBitmap.cs
+++ Packages/com.pg649.worldgeneration/Runtime/Terrain Generation/Scripts/EnvironmentGeneratorBitmap.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using Unity.AI.Navigation;
 using UnityEngine;
 
 public class EnvironmentGeneratorBitmap {
@@ -36,13 +37,25 @@ public class EnvironmentGeneratorBitmap {
         for (int y = 0; y < size; y++) {
             for (int x = 0; x < size; x++) {
                 heights[y, x] = heightmapMask[y, x] ? 0f : .8f;
-                if(ivbm[y,x]) heights[y, x] = 0.5f;
+                if (ivbm[y, x]) heights[y, x] = 0.5f;
                 // heights[y, x] = heightmapMask[y, x] ? Mathf.Clamp01(Mathf.PerlinNoise(y / FREQUENCY, x / FREQUENCY) * AMPLITUDE) : 1f;
                 // heights[y, x] += Mathf.Clamp01(Mathf.PerlinNoise(y / FREQUENCY, x / FREQUENCY) * AMPLITUDE);
             }
         }
 
-        /*System.Diagnostics.Stopwatch sw = new();
+
+        //AddNoise(heights);
+
+        for (int i = 0; i < 5; i++) HeightmapTransforms.ApplyFilter(heights, HeightmapTransforms.AverageFilter(3, 3), heightmapMask, invertMask: true);
+
+        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight: .4f, mask: heightmapMask);
+        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight: .4f, scale: .6f, mask: ivbm);
+        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight: .1f, scale: .4f, mask: heightmapMask.ZipMap(ivbm, (x, y) => !x && !y));
+
+
+        //heights = TerrainTransforms.Twirl(heights, 1f);
+
+        System.Diagnostics.Stopwatch sw = new();
 
         sw.Start();
 
@@ -53,19 +66,7 @@ public class EnvironmentGeneratorBitmap {
         sw.Stop();
 
         Debug.Log(sw.Elapsed);
-        AddNoise(heights);
-*/
-        for(int i = 0; i < 5; i++) HeightmapTransforms.ApplyFilter(heights, HeightmapTransforms.AverageFilter(3,3), heightmapMask, invertMask : true);
 
-        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight : .4f, mask : heightmapMask);
-        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight : .4f, scale : .6f, mask : ivbm);
-        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight : .1f, scale : .4f, mask : heightmapMask.ZipMap(ivbm, (x,y) => !x && !y));
-       
-
-        //heights = TerrainTransforms.Twirl(heights, 1f);
-
-
-        // SmoothAlongMask(heights);
         //AddNoise(heights);
         //heights = TerrainBlur.ApplyBlur(heights, 5);
         // for (int i = 0; i < 3; i++) {
@@ -73,68 +74,15 @@ public class EnvironmentGeneratorBitmap {
         // }
         terrain.terrainData.SetHeights(0, 0, heights);
         terrain.terrainData.size = new Vector3(size, gen_settings.Depth, size);
-        // Terrain.Flush();
-        // Terrain.terrainData.size = new Vector3(settings.TerrainSize, settings.Depth, settings.TerrainSize);
 
         terrain.Flush();
+        AddNavMesh(go);
     }
-
-    // public void SmoothAlongMask(float[,] heights) {
-    //     // TODO increase speed by blurring into a new array on GPU via compute shaders -> control via passes -> no need for lookahead/radius(maybe implementradius as lookahead later)
-
-    //     //determine borderpixels of heightmap mask -> pixels of inner border are calculated
-    //     List<Tuple<int, int>> borderCoords = new();
-    //     // padded by 1 as border coordinates of the terrain do not qualify as borderpixels
-    //     for (int y = 1; y < size - 1; y++) {
-    //         for (int x = 1; x < size - 1; x++) {
-    //             if (heightmapMask[y, x] != heightmapMask[y - 1, x] // bottom
-    //             || heightmapMask[y, x] != heightmapMask[y + 1, x] // top
-    //             || heightmapMask[y, x] != heightmapMask[y, x - 1] // left
-    //             || heightmapMask[y, x] != heightmapMask[y, x + 1] //right
-    //             ) {
-    //                 borderCoords.Add(new(x, y));
-    //             }
-    //         }
-    //     }
-    //     // Debug.Log(borderCoords.Count);
-    //     // var smoothRadius = 3;
-    //     // var smoothPasses = 20;
-    //     // for (int n = 0; n < smoothPasses; n++) {
-    //     //     foreach (var (x, y) in borderCoords) {
-    //     //         for (int i = 0; i < smoothRadius; i++) {
-    //     //             for (int j = 0; j < smoothRadius; j++) {
-    //     //                 if (x - j < 1 || y - i < 1 || x + j > size - 2 || y + j > size - 2) continue; // dont smooth too close to edge of terrain
-    //     //                 heights[y + i, x + j] = Mathf.Clamp01(GetSmoothedValue(x + j, y + i, heights, true));
-    //     //                 heights[y - i, x - j] = Mathf.Clamp01(GetSmoothedValue(x - j, y - i, heights, true));
-    //     //             }
-    //     //         }
-
-    //     //     }
-    //     // }
-    //     // for (int y = smoothRadius; y < size - smoothRadius; y++) {
-    //     //     for (int x = smoothRadius; x < size - smoothRadius; x++) {
-    //     //         // smooth any position
-    //     //         //TODO only consider edges of rooms
-    //     //         for (int i = 0; i < smoothRadius; i++) {
-    //     //             for (int j = 0; j < smoothRadius; j++) {
-    //     //                 heights[y + i, x + j] = Mathf.Clamp01(GetSmoothedValue(x + j, y + i, heights, true));
-    //     //                 heights[y - i, x - j] = Mathf.Clamp01(GetSmoothedValue(x - j, y - i, heights, true));
-    //     //             }
-    //     //         }
-    //     //     }
-    //     // // }
-    //     // for (int n = 0; n < smoothPasses; n++) {
-    //     //     for (int y = 1; y < size - 1; y++) {
-    //     //         for (int x = 1; x < size - 1; x++) {
-    //     //             // smooth any position
-    //     //             //TODO only consider edges of rooms
-    //     //             heights[y, x] = Mathf.Clamp01(GetSmoothedValue(x, y, heights, true));
-    //     //             heights[y, x] = Mathf.Clamp01(GetSmoothedValue(x, y, heights, true));
-
-    //     //         }
-    //     //     }
-    //     // }
-    // }
+    private void AddNavMesh(GameObject go) {
+        NavMeshSurface nms = go.GetComponent<NavMeshSurface>();
+        if (nms == null) nms = go.AddComponent<NavMeshSurface>();
+        nms.BuildNavMesh();
+    }
 
     public void AddNoise(float[,] heights) {
         for (int y = 0; y < size; y++) {
@@ -143,24 +91,5 @@ public class EnvironmentGeneratorBitmap {
             }
         }
     }
-
-    // private float GetSmoothedValue(int x, int y, float[,] heights, bool strongSmoothing) {
-    //     // calculate neighbours -> pos > 0 this works
-    //     var heightTopLeft = heights[y - 1, x - 1];
-    //     var heightTop = heights[y - 1, x];
-    //     var heightTopRight = heights[y - 1, x + 1];
-
-    //     var heightLeft = heights[y, x - 1];
-    //     var height = heights[y, x];
-    //     var heightRight = heights[y, x + 1];
-
-    //     var heightBottomLeft = heights[y + 1, x - 1];
-    //     var heightBottom = heights[y + 1, x];
-    //     var heightBottomRight = heights[y + 1, x + 1];
-
-    //     float mean = (heightTopLeft + heightTop + heightTopRight + heightLeft + heightRight + heightBottomLeft + heightBottom + heightBottomRight) / 8 - height;
-
-    //     return strongSmoothing ? height + mean : height + mean / 2;
-    // }
 
 }
