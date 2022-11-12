613235659f1e0b6dc7e1b6814278321b878c5193
kb1t <kb1t@users.noreply.github.com>
2022-11-12 15:07:19 +0100

    fixed the terrain borders

--- Packages/com.pg649.worldgeneration/Runtime/SP Terrain/Heightmap.cs
+++ Packages/com.pg649.worldgeneration/Runtime/SP Terrain/Heightmap.cs
@@ -1,57 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using System;
-public class Heightmap
-{   
+using UnityEngine;
+public class Heightmap {
     private int heightScale;
     private int size;
     private float[,] heights;
 
-    public Heightmap(int size, int heightScale = 100){
+    public Heightmap(int size, int heightScale = 100) {
         this.size = size;
         this.heightScale = heightScale;
-        this.heights = new float[size,size];
+        this.heights = new float[size, size];
     }
-    
-    public void SetByMask(Mask mask, float trueValue, float falseValue = -1){
-        HeightmapTransforms.SetByMask(heights, mask, trueValue/heightScale, falseValue : falseValue == -1 ? -1 : falseValue/heightScale);
+
+    public void SetByMask(Mask mask, float trueValue, float falseValue = -1) {
+        HeightmapTransforms.SetByMask(heights, mask, trueValue / heightScale, falseValue: falseValue == -1 ? -1 : falseValue / heightScale);
     }
-    public void PerlinNoise(float maxAddedHeight = 1, float scale = .1f, Mask mask = null, int fractalRuns = 1){
-        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight : maxAddedHeight/heightScale, scale : scale, mask : mask, fractalRuns : fractalRuns);
+    public void PerlinNoise(float maxAddedHeight = 1, float scale = .1f, Mask mask = null, int fractalRuns = 1) {
+        HeightmapTransforms.ApplyPerlinNoise(heights, maxAddedHeight: maxAddedHeight / heightScale, scale: scale, mask: mask, fractalRuns: fractalRuns);
     }
-    public void AverageFilter(Mask mask = null, int numberOfRuns = 1){
+    public void AverageFilter(Mask mask = null, int numberOfRuns = 1) {
         // System.Diagnostics.Stopwatch sw = new();
         // sw.Restart();
-        for(int i = 0; i < numberOfRuns; i++){
-            HeightmapTransforms.ApplyFilter(heights, HeightmapTransforms.AverageFilter(3,3), mask : mask);
+        for (int i = 0; i < numberOfRuns; i++) {
+            HeightmapTransforms.ApplyFilter(heights, HeightmapTransforms.AverageFilter(3, 3), mask: mask);
         }
         // sw.Stop();
         // Debug.Log("Runtime AverageFilter CPU:\t " + sw.Elapsed);
         // sw.Reset();
     }
     public void AverageFilterGPU(ComputeShader averageShader, Mask mask = null, int numberOfRuns = 1) {
-        // System.Diagnostics.Stopwatch sw = new();
-        // sw.Restart();
+        //System.Diagnostics.Stopwatch sw = new();
+        //sw.Restart();
         TerrainShader.AverageFilterGPU3x3(averageShader, input: heights, mask: mask, passes: numberOfRuns);
-        // sw.Stop();
-        // Debug.Log("Runtime AverageFilter GPU:\t " + sw.Elapsed);
-        // sw.Reset();
+        //sw.Stop();
+        //Debug.Log("Runtime AverageFilter GPU:\t " + sw.Elapsed);
+        //sw.Reset();
     }
-    public void Power(float power, Mask mask = null){
-        MapI(f => Mathf.Pow(f,power), mask);
+    public void Power(float power, Mask mask = null) {
+        MapI(f => Mathf.Pow(f, power), mask);
     }
 
-    public void MapI(Func<float,float> f , Mask mask = null){
-        for(int i = 0; i < size; i++){
-            for(int j = 0; j < size; j++){
-                if(mask == null || mask[i,j]) heights[i,j] = f(heights[i,j]);
+    public void MapI(Func<float, float> f, Mask mask = null) {
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (mask == null || mask[i, j]) heights[i, j] = f(heights[i, j]);
             }
         }
     }
-    
 
-    public void AddTerrainToGameObject(GameObject go){
+
+    public void AddTerrainToGameObject(GameObject go) {
         Terrain terrain = go.AddComponent<Terrain>();
         terrain.terrainData = new TerrainData();
         terrain.materialTemplate = new Material(Shader.Find("Nature/Terrain/Diffuse"));
--- Packages/com.pg649.worldgeneration/Runtime/Terrain Generation/Scripts/TerrainShader.cs
+++ Packages/com.pg649.worldgeneration/Runtime/Terrain Generation/Scripts/TerrainShader.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using UnityEngine;
 
 public static class TerrainShader {
@@ -97,68 +97,80 @@ public static class TerrainShader {
         if (computeShader == null) throw new ArgumentNullException("Average Shader is not set in settings object");
 
         int side_length = input.GetLength(0);
-        //TODO its somehow broken around the borders
-        // // exclude borders and linearize
-        const int radius = 1;
-        int shrinked_side_length = side_length - 2;
-        int flat_arr_size = (side_length - 2) * (side_length - 2);
-        // int flat_arr_size = side_length * side_length;
-        float[] heightsFlat = new float[flat_arr_size];
-        int[] maskFlat = new int[flat_arr_size];
-        //TODO mask instead of using int use a ByteArray and set those accordingly maybe
-        //TODO improve speed by packing float values into float4 packages
-        for (int i = 1; i < side_length - 1; i++) {
-            for (int j = 1; j < side_length - 1; j++) {
-                // if (mask == null || (mask[i, j] && !invertMask) || (invertMask && !mask[i, j])) {
-                maskFlat[(i - 1) * shrinked_side_length + (j - 1)] = mask[i, j] ? 1 : 0;
-                heightsFlat[(i - 1) * shrinked_side_length + (j - 1)] = input[i, j];
-                // } else {
-                //     maskFlat[i * side_length + j] = 1;
-                //     if (add) heightsFlat[i * side_length + j] = input[i, j];
-                // }
-                // heightsFlat[i * shrinked_side_length + j] = input[i, j];
-                // maskFlat[i * shrinked_side_length + j] = (mask == null || (mask[i, j] && !invertMask) || (invertMask && !mask[i, j])) ? true : false;
+
+        // linearize heights and mask
+        float[] heightsFlat = new float[side_length * side_length];
+        int[] maskFlat = new int[side_length * side_length];
+
+        for (int i = 0; i < side_length; i++) {
+            for (int j = 0; j < side_length; j++) {
+                maskFlat[i * side_length + j] = mask[i, j] ? 1 : 0;
+                heightsFlat[i * side_length + j] = input[i, j];
             }
         }
 
-        ComputeBuffer maskBuffer = new ComputeBuffer(flat_arr_size, sizeof(int));
-        maskBuffer.SetData(maskFlat, 0, 0, flat_arr_size);
 
-        ComputeBuffer inputBuffer = new ComputeBuffer(flat_arr_size, sizeof(float));
-        inputBuffer.SetData(heightsFlat, 0, 0, flat_arr_size);
+        //TODO try mask instead of using int use a ByteArray and set those accordingly maybe
+        //TODO improve speed by packing float values into float4 packages
+        ComputeBuffer maskBuffer = new ComputeBuffer(side_length * side_length, sizeof(int));
+        maskBuffer.SetData(maskFlat);
 
-        ComputeBuffer outputBuffer = new ComputeBuffer(flat_arr_size, sizeof(float));
+        ComputeBuffer inputBuffer = new ComputeBuffer(side_length * side_length, sizeof(float));
 
-        int kernelID = multiplyFilter ? computeShader.FindKernel("AverageFilterMultiply3x3") : computeShader.FindKernel("AverageFilterMultiplyAdd3x3");
+        ComputeBuffer outputBuffer = new ComputeBuffer(side_length * side_length, sizeof(float));
+
+        // prepare buffer zeroing kernel
+        int zeroingKernel = computeShader.FindKernel("SetBufferZero");
+        computeShader.GetKernelThreadGroupSizes(zeroingKernel, out var zeroingThreadGroupSize, out _, out _);
+        int zeroingThreadGroups = Mathf.CeilToInt((side_length) * (side_length) / zeroingThreadGroupSize);
+
+        int kernelID;
+        if (add)
+            kernelID = multiplyFilter ? computeShader.FindKernel("AverageFilterMultiplyKeep3x3") : computeShader.FindKernel("AverageFilterAddKeep3x3");
+        else
+            kernelID = multiplyFilter ? computeShader.FindKernel("AverageFilterMultiply3x3") : computeShader.FindKernel("AverageFilterAdd3x3");
+
 
         computeShader.GetKernelThreadGroupSizes(kernelID, out var threadGroupSize, out _, out _);
-        int threadGroups = Mathf.CeilToInt(flat_arr_size / threadGroupSize);
+        int threadGroups = Mathf.CeilToInt((side_length) * (side_length) / threadGroupSize);
 
-        computeShader.SetInt("_size", shrinked_side_length);
-        computeShader.SetBool("_add", add);
+        computeShader.SetInt("_size_safe", (side_length - 2));
+        computeShader.SetInt("_size_input", side_length);
+        computeShader.SetInt("_end_of_safe_zone", (side_length - 2) * (side_length - 2));
         computeShader.SetBuffer(kernelID, "_mask", maskBuffer);
 
+        // populate input
+        inputBuffer.SetData(heightsFlat, 0, 0, side_length * side_length);
+
         for (int n = 0; n < passes; n++) {
-            // alternate between runs
-            if (n % 2 == 0) {
+            if (n % 2 == 0) { // alternate between runs
+                // zero current output buffer
+                computeShader.SetBuffer(zeroingKernel, "_zero_buffer", outputBuffer);
+                computeShader.Dispatch(zeroingKernel, zeroingThreadGroups, 1, 1);
+
+                // set input/output buffers accordingly
                 computeShader.SetBuffer(kernelID, "_input", inputBuffer);
                 computeShader.SetBuffer(kernelID, "_output", outputBuffer);
-
                 computeShader.Dispatch(kernelID, threadGroups, 1, 1);
             } else {
+                // zero current output buffer
+                computeShader.SetBuffer(zeroingKernel, "_zero_buffer", inputBuffer);
+                computeShader.Dispatch(zeroingKernel, zeroingThreadGroups, 1, 1);
+
+                // set input/output buffers accordingly
                 computeShader.SetBuffer(kernelID, "_input", outputBuffer);
                 computeShader.SetBuffer(kernelID, "_output", inputBuffer);
-
                 computeShader.Dispatch(kernelID, threadGroups, 1, 1);
             }
         }
+
         // get the data from the correct buffer
         if (passes % 2 == 1) outputBuffer.GetData(heightsFlat); else inputBuffer.GetData(heightsFlat);
 
         // expand result
-        for (int i = 1; i < side_length - 1; i++) {
-            for (int j = 1; j < side_length - 1; j++) {
-                input[i, j] = heightsFlat[(i - 1) * shrinked_side_length + (j - 1)];
+        for (int i = 0; i < side_length; i++) {
+            for (int j = 0; j < side_length; j++) {
+                input[i, j] = heightsFlat[i * (side_length) + j];
             }
         }
 
